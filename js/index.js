70e422f03b89fef3baaa6a5f05f7e789a86fb0ad
John Doe <alirezanjprogrammer110@gmail.com>
2021-09-12 16:21:07 +0430

    save localstorage , dlete and cumplid

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const inputText=document.querySelector(".input-text")
 const btn=document.querySelector(".plus")
 const todolist=document.querySelector(".todolist")
-
+// add todo
 btn.addEventListener("click",e=>{
     e.preventDefault();
     const todoitem=document.createElement("div")
@@ -19,6 +19,31 @@ btn.addEventListener("click",e=>{
 </li>`
 todoitem.innerHTML=newTodo;
 todolist.appendChild(todoitem)
+savedLocalstrage(inputText.value)
 inputText.value="";
     
 })
+// dlete and cumplid todo
+todolist.addEventListener("click",e=>{
+    const classlist=[...e.target.classList]
+    const item=e.target
+   if(classlist[1]==="fa-trash"){
+       const todo=item.parentElement.parentElement
+       todo.remove();
+   }
+   if(classlist[1]==="fa-check"){
+       const todo=item.parentElement.parentElement
+       todo.classList.toggle("cumplid")
+   }
+ 
+    
+    
+   
+  
+})
+// saved todo
+function savedLocalstrage(todo){
+    const saved=localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")):[]
+    saved.push(todo)
+    return localStorage.setItem("todos",JSON.stringify(saved))
+}
