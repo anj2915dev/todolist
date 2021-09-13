cbc3d1b99e4309d74b4f403ce025351c75938806
John Doe <alirezanjprogrammer110@gmail.com>
2021-09-14 02:34:20 +0430

    filter todo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const inputText = document.querySelector(".input-text")
 const btn = document.querySelector(".plus")
 const todolist = document.querySelector(".todolist")
+const select = document.querySelector("#lst")
 // add todo
 btn.addEventListener("click", e => {
     e.preventDefault();
@@ -26,6 +27,8 @@ btn.addEventListener("click", e => {
 // dlete and cumplid todo
 todolist.addEventListener("click", e => {
     const classlist = [...e.target.classList]
+    console.log(e.target.classList)
+
     const item = e.target
     if (classlist[1] === "fa-trash") {
         const todo = item.parentElement.parentElement
@@ -34,8 +37,11 @@ todolist.addEventListener("click", e => {
 
     }
     if (classlist[1] === "fa-check") {
-        const todo = item.parentElement.parentElement
-        todo.classList.toggle("cumplid")
+        const todo = item.parentElement.parentElement.parentElement.parentElement
+        const todo2 = item.parentElement.parentElement
+        todo.classList.toggle("cumplited");
+        todo2.classList.toggle("cumplited");
+       
     }
 
 
@@ -43,15 +49,51 @@ todolist.addEventListener("click", e => {
 
 
 })
+select.addEventListener("click", cumplidUncumplid);
+function cumplidUncumplid(a) {
+const itemtodo=[...todolist.childNodes]
+itemtodo.forEach(item=>{
+    switch(a.target.value){
+        case("all"):
+      
+        item.style.display = "flex"
+        break;
+        case("cumplid"):
+        if(item.classList.contains("cumplited")){
+        item.style.display = "flex"
+
+        }
+        else{
+        item.style.display = "none"
+
+        }
+        break;
+        case("uncumplid"):
+        if(!item.classList.contains("cumplited")){
+        item.style.display = "flex"
+
+        }
+        else{
+        item.style.display = "none"
+
+        }
+    }
+})
+
+
+
+    
+
+}
 // saved todo
 function savedLocalstrage(todo) {
     const saved = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []
     saved.push(todo)
     return localStorage.setItem("todos", JSON.stringify(saved))
 }
-document.addEventListener("DOMContentLoaded",getlocalstrage)
-function getlocalstrage(e){
-    const savedlocal=localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")):[]
+document.addEventListener("DOMContentLoaded", getlocalstrage)
+function getlocalstrage(e) {
+    const savedlocal = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []
     savedlocal.forEach(todo => {
         const todoitem = document.createElement("div")
         todoitem.classList.add("todos")
@@ -69,10 +111,12 @@ function getlocalstrage(e){
         todoitem.innerHTML = newTodo;
         todolist.appendChild(todoitem)
     });
-  
+
 }
-function removeTodosLocalstoreg(t){
-    const todo=localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")):[]
-    const filterTodo=todo.filter(item=>item!==t.children[0].innerText)
-    localStorage.setItem("todos",JSON.stringify(filterTodo))
+// remove in localstorage
+function removeTodosLocalstoreg(t) {
+    const todo = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []
+    const filterTodo = todo.filter(item => item !== t.children[0].innerText)
+    localStorage.setItem("todos", JSON.stringify(filterTodo))
 }
+
